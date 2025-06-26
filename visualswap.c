ae36877c0919641baabbcc99bb17abb040efe11f
varshithabandi2907 <varshithabandi29@gmail.com>
2025-06-26 22:31:36 +0530

    Create visualswap.c

---
+++ visualswap.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter a,b and c values:");
+scanf("%d %d %d",&a, &b, &c);
+printf("Before swap a= %d , b= %d and c=%d",a,b,c);
+printf("\n After swap a= %d , b= %d and c=%d",c,b,a);
+}
