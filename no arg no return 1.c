1bfddb0a55fbd45818cc6bbfd8f10abe03d64e88
varshithabandi2907 <varshithabandi29@gmail.com>
2025-06-27 00:01:09 +0530

    Create no arg no return 1.c

---
+++ no arg no return 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void number(){
+    int num;
+    printf("enter a number:");
+    scanf("%d",num);
+    printf("you have enter %d \n",num);
+}
+int main(){
+   number();
+    return 0;
+}
