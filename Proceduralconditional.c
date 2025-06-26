8cfa77016b130459ef596c93c3b618ba0305612a
varshithabandi2907 <varshithabandi29@gmail.com>
2025-06-26 22:40:19 +0530

    Create Proceduralconditional.c

---
+++ Proceduralconditional.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter a number:");
+    scanf("%d",&num);
+    if(num>0)
+    {
+        printf("the number is positive");
+    }
+   else if(num<0)
+   {
+        printf("the number is negative");
+    }
+    else{
+        printf("the number is zero");
+    }
+    return 0;
+}
