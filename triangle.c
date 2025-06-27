4bb8d5ab0f871e68d10739e9209a38dcf097dad0
varshithabandi2907 <varshithabandi29@gmail.com>
2025-06-27 19:57:46 +0530

    Create triangle.c

---
+++ triangle.c
@@ -0,0 +1,19 @@
+\* n-i => triangle*\
+#include <stdio.h>
+int main() {
+    char ch='A';
+    int i,j,k,n;
+    printf("enter the number of rows");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(k=1;k<=i;k++){
+            printf("%c ",ch);
+            ch++;
+            }
+            printf("\n");
+    }       
+    return 0;
+}
